1757af0d06768ac723033efbfe98800477c6f42a
Sandra Moen <sandramoen01@gmail.com>
2019-06-19 10:02:24 +0200

    #1 Kotlin + Android setup

--- desktop/src/no/sandramoen/suicide/desktop/DesktopLauncher.java
+++ desktop/src/no/sandramoen/suicide/desktop/DesktopLauncher.java
@@ -2,11 +2,20 @@ package no.sandramoen.suicide.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import no.sandramoen.suicide.Launcher;
+
+import no.sandramoen.suicide.SuicideGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Launcher(), config);
+
+		config.title = "Suicide?";
+		int scale = 4; // resolution of lg g4 is 1440 x 2560 (in portrait mode)
+		config.width = (int)1440 / scale;
+		config.height = (int)2560 / scale;
+		config.resizable = false;
+
+		new LwjglApplication(new SuicideGame(), config);
+		/*new LwjglApplication(new Launcher(), config);*/
 	}
 }
